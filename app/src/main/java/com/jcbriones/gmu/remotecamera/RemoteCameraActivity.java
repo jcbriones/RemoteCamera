e70e4e0653ba1b7fe2c01a9d7efa315a8ad6b5d2
Jc Briones <mail@jcbriones.com>
2017-11-30 13:36:25 -0500

    Initial commit

---
+++ app/src/main/java/com/jcbriones/gmu/remotecamera/RemoteCameraActivity.java
@@ -0,0 +1,13 @@
+package com.jcbriones.gmu.remotecamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemoteCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_camera);
+    }
+}
